db4894ad2668551510ebecf444d71f97b3480a8a
Thomas Meulendijks <t.meulendijks@outlook.com>
2019-02-28 09:25:20 +0100

    init file structure

---
+++ modules/facial-detection.py
@@ -0,0 +1,27 @@
+import face_recognition
+import picamera
+import numpy as np
+
+# Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+
+while True:
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format="rgb")
+
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+
+    for face_location in face_locations:
+        # Print the location of each face in this image
+        top, right, bottom, left = face_location
+        print("A face is located at pixel location "
+                "Top: {}, Left: {}, Bottom:{}, Right: {}"
+                .format(top, left, bottom, right))
+
