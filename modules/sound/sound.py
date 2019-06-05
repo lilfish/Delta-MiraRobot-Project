eef704e888cd425f4cf952f342a4ffe6795331f5
Thomas Meulendijks <t.meulendijks@tuta.io>
2019-06-05 16:09:43 +0200

    Allways set analogue output for raspberrypi

--- modules/sound/sound.py
+++ modules/sound/sound.py
@@ -3,13 +3,16 @@ import os
 # Select media player change to mpg123 for use on PI
 player = "mpv --no-audio-display "
 
+def setup():
+    # set output to id 1. id 1 = analogue jack, id 2 = hdmi
+    os.system("amixer cset numid=3 1")
 def play(audiofile):
-	os.system(player + audiofile)
+    os.system(player + audiofile)
 def happy():
-	play("./data/happy.mp3")
+    play("./data/happy.mp3")
 def sad():
-	play("./data/sad.mp3")
+    play("./data/sad.mp3")
 def cheerful():
-	play("./data/cheerful.mp3")
+    play("./data/cheerful.mp3")
 def excited():
-	play("./data/excited.mp3")
+    play("./data/excited.mp3")
